b9f3dc6e21f27c7fb14db09ca7535c6813d21b05
Wise Munene Mutuku <wisemunene99@gmail.com>
2025-11-13 09:09:54 +0300

    done

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-def return_evens(num_list):
-    pass
+def return_evens(num_list): # Returns a list of even numbers from the input list
+    return [n for n in num_list if n % 2 == 0]
 
 def make_exclamation(sentence_list):
-    pass
+    return [s + '!' for s in sentence_list] # Appends an exclamation mark to each string in the input list
